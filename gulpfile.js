991180d9d27cf3fa9aa27a1a048dfc9b2ad7bb10
cs <bartus.csongor@gmail.com>
2015-02-15 20:24:10 +0200

    Cleaning up the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -10,34 +10,29 @@ var gulp = require('gulp'),
 
     swig = require('gulp-swig'),
     data = require('gulp-data'),
-    fm = require('front-matter'),
-    fs = require('fs'),
-
-    minifycss = require('gulp-minify-css'),
-    uglify = require('gulp-uglify');
+    fs = require('fs');
 
 
 // Folder structure
 var paths = {
+  // the final destination folder
+  dest: 'dist',
+
+  // the source of all swig files
   swig_src: 'app/**/*.swig',
+
+  // the destination of all compiles swig files
   swig_dest: 'app',
-  css: 'app/components/pages/*.css',
-  js: 'app/components/**/*.js',
-  styles: 'dist/assets/styles',
-  scripts: 'dist/assets/scripts',
-  assets_src: 'app/assets/**/*',
-  assets_dest: 'dist/assets',
-  pages: 'app/components/pages/*.html',
-  site: 'dist',
-  home: 'dist/home',
-  watch: ['components/**/*.swig', 'components/pages/*.{js,css}']
+
+  // watch these files for changes
+  watch: ['app/**/*.swig', 'app/**/*.json']
 };
 
 
 
 // Swig
 // - compile a .swig file with external JSON data into HTML or SCSS
-// - the generated file will be saved into the same directory as source file
+// - the generated file will be saved into the same directory as the source file
 // - it works also when there is no JSON file
 //
 // - the .swig file has to have two prefixes, the first prefix is the output format (.scss, .html)
@@ -85,87 +80,9 @@ gulp.task('swig', function() {
 
 
 
-// Swig
-// - compile a .swig file with YAML front matter into HTML
-gulp.task('swig2', function() {
-  return gulp.src(paths.swig)
-    .pipe(data(function(file) {
-      var content = fm(String(file.contents));
-      file.contents = new Buffer(content.body);
-      return content.attributes;
-    }))
-    .pipe(swig({
-      defaults: {
-        cache: false
-      }
-    }))
-    .pipe(rename({ extname: '.html' }))
-    .pipe(gulp.dest(paths.components));
-});
-
-
-// Styles
-// - move all .css files from components/pages to site/assets/styles
-// - .css is created from .scss by Compass not Gulp
-// - Compass is used instead of Autoprefixer because of it's well working globbing feature
-gulp.task('styles', function() {
-  return gulp.src(paths.css)
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(minifycss())
-    .pipe(gulp.dest(paths.styles));
-});
-
-
-// Scripts
-// - collect all .js files into all.js, then minify into all.min.js, then move to site/assets/scripts
-gulp.task('scripts', function() {
-  return gulp.src(paths.js)
-    .pipe(concat('all.js'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
-    .pipe(gulp.dest(paths.scripts));
-});
-
-
-// Assets
-// - move all assets from assets/ to site/assets/
-gulp.task('assets', function() {
-  return gulp.src(paths.assets_src)
-    .pipe(gulp.dest(paths.assets_dest));
-});
-
-
-// Pages
-// - compacting pages
-// - ex: pages/home.html => home/index.html
-gulp.task('pages', function() {
-  return gulp.src(paths.pages)
-    .pipe(rename(function(path) {
-      path.dirname = path.basename;
-      path.basename = 'index';
-    }))
-    .pipe(gulp.dest(paths.site))
-  ;
-});
-
-
-
-// Home
-// - making a homepage from an existing page
-// ex: home/index.html => index.html
-gulp.task('home', function(cb) {
-  gulp.src(paths.home + '/index.html')
-    .pipe(rename('index.html'))
-    .pipe(gulp.dest(paths.site))
-  del([paths.home]);
-  cb();
-});
-
-
-
-// Clean site/**
+// Clean destination folder
 gulp.task('clean', function(cb) {
-  del([paths.site + '/**/*']);
+  del([paths.dest + '/**/*']);
   cb();
 });
 
@@ -179,11 +96,6 @@ gulp.task('default', function(cb) {
   runSequence(
     'clean',
     'swig',
-    'styles',
-    'scripts',
-    'assets',
-    'pages',
-    'home',
     cb
   );
 });
@@ -193,7 +105,7 @@ gulp.task('default', function(cb) {
 gulp.task('server', function(cb) {
   browserSync({
     server: {
-      baseDir: paths.site
+      baseDir: paths.dest
     }
   });
 
