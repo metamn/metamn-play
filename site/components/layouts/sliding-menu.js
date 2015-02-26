a3f4d487d892cdd615272f1e0cf236f981907cac
cs <bartus.csongor@gmail.com>
2015-02-26 17:23:40 +0200

    Ready to set up the vertical grid

--- site/components/layouts/sliding-menu.js
+++ site/components/layouts/sliding-menu.js
@@ -1,6 +1,6 @@
 var container = document.querySelector('#base');
 var trigger = document.querySelector('#base #logo');
-var backButton = document.querySelector('#base #menu #logo');
+var backButton = document.querySelector('#base #logo');
 
 function onViewChange(evt) {
   container.classList.toggle('view-change');
