9a74dd70b8401a03a4c0fe12cd5ad263cc417963
cs <bartus.csongor@gmail.com>
2015-03-01 21:01:15 +0200

    Scrolling to a predefined section works

--- site/components/layouts/full-page-scroll.js
+++ site/components/layouts/full-page-scroll.js
@@ -1,5 +1,22 @@
 // Scrolling a full page instead of the usual scroll
 (function() {
+
+  // Saving the top of each section into an array
+  // - later we will scroll to these values
+  // - http://stackoverflow.com/questions/25630035/javascript-getboundingclientrect-changes-while-scrolling
+  var sectionTops = function() {
+    ret = [];
+
+    sections = document.querySelectorAll('.home #content section');
+    for (var i = 0; i < sections.length; i++ ) {
+      position = sections[i].getBoundingClientRect();
+      ret.push(position.top + window.scrollY);
+    }
+
+    return ret;
+  }
+
+
   // Hijacking the original scroller
   // - add throttling
   // - http://greensock.com/forums/topic/11155-how-to-optimize-tweenlite-on-scroll/
@@ -17,7 +34,13 @@
   function scrollComplete() {
     console.log("scrolling done");
 
-    var sectionTops = [0, 1117, "2234"];
-    TweenLite.to(window, 0.5, { scrollTo: sectionTops[1], ease:Power2.easeOut });
+    tops = sectionTops();
+    current = currentSection();
+    TweenLite.to(window, 0.5, { scrollTo: tops[current], ease:Power2.easeOut });
+  }
+
+  // Get the current section where to scroll
+  function currentSection() {
+    return 1;
   }
 })();
