78e29e02ab9a3a2699aa2cf6d66966a6d4d39970
cs <bartus.csongor@gmail.com>
2015-03-01 18:34:55 +0200

    Greensock Tweenlite added

--- site/components/layouts/full-page-scroll.js
+++ site/components/layouts/full-page-scroll.js
@@ -1,1 +1,22 @@
-onePageScroll(".home #content");
+// Scrolling a full page instead of the usual scroll
+var fullPageScroll = function() {
+
+  // Hijacking the original scroller
+  // - add throttling
+  // - http://greensock.com/forums/topic/11155-how-to-optimize-tweenlite-on-scroll/
+  var scrollTimeout;
+  window.onscroll = scrollHandler;
+
+  function scrollHandler(event) {
+    console.log("onscroll");
+    clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(scrollComplete, 200);
+  }
+
+  // Do the modified scroll
+  function scrollComplete() {
+    console.log("scrolling done");
+  }
+
+}
+fullPageScroll();
