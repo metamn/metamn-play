b7bd7f8c921ce626d7180b5311e2cd995a640850
cs <bartus.csongor@gmail.com>
2015-03-01 13:05:46 +0200

    Scrolling control function added

--- site/components/layouts/full-page-scroll.js
+++ site/components/layouts/full-page-scroll.js
@@ -1,35 +1,16 @@
 // Full page scrolling
-// - supporting mouse, keyboard and swipe events with no throttle / debounce
 // - debouncing: http://codepen.io/GreenSock/pen/AwHmy
-// - mouse: http://www.sitepoint.com/html5-javascript-mouse-wheel/
 
-
-// Mouse + Touchpad
-//
-
-// Listen to the scroll event
-var body = document.querySelector("body");
-if (body.addEventListener) {
-	// IE9, Chrome, Safari, Opera
-	body.addEventListener("mousewheel", MouseWheelHandler, false);
-	// Firefox
-	body.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
-} else {
-  // IE 6/7/8
-  body.attachEvent("onmousewheel", MouseWheelHandler);
-}
-
-// Debounce & get scrolling direction
 var scrollTimeout;
-function MouseWheelHandler(e) {
-	// cross-browser wheel delta
-	var e = window.event || e; // old IE support
-	var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-  // Debouncing ...
-  clearTimeout(scrollTimeout);
-  scrollTimeout = setTimeout(scrollComplete(delta), 5000);
-}
 
-function scrollComplete(direction) {
-  console.log("delta: " + direction);
+window.onscroll = doScroll;
+
+function doScroll() {
+  console.log("onscroll");
+  clearTimeout(scrollTimeout);
+  scrollTimeout = setTimeout(scrollComplete, 200);
+}
+function scrollComplete() {
+  // code to run after scrolling here
+  console.log("scrolling done");
 }
