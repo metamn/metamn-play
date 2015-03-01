918f70936e5015c3bc745812d95bd13ee97331a1
cs <bartus.csongor@gmail.com>
2015-03-01 18:19:01 +0200

    Restarting the entire scrolling

--- site/components/layouts/full-page-scroll.js
+++ site/components/layouts/full-page-scroll.js
@@ -1,16 +1,1 @@
-// Full page scrolling
-// - debouncing: http://codepen.io/GreenSock/pen/AwHmy
-
-var scrollTimeout;
-
-window.onscroll = doScroll;
-
-function doScroll() {
-  console.log("onscroll");
-  clearTimeout(scrollTimeout);
-  scrollTimeout = setTimeout(scrollComplete, 200);
-}
-function scrollComplete() {
-  // code to run after scrolling here
-  console.log("scrolling done");
-}
+onePageScroll(".home #content");
