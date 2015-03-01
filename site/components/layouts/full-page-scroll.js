6ef7220a352605ce8eec75b168b71c26d5e43202
cs <bartus.csongor@gmail.com>
2015-03-01 20:04:23 +0200

    Greensock TweenLite ScrollTo works prefect, scheleton ready

--- site/components/layouts/full-page-scroll.js
+++ site/components/layouts/full-page-scroll.js
@@ -1,6 +1,5 @@
 // Scrolling a full page instead of the usual scroll
-var fullPageScroll = function() {
-
+(function() {
   // Hijacking the original scroller
   // - add throttling
   // - http://greensock.com/forums/topic/11155-how-to-optimize-tweenlite-on-scroll/
@@ -13,10 +12,12 @@ var fullPageScroll = function() {
     scrollTimeout = setTimeout(scrollComplete, 200);
   }
 
-  // Do the modified scroll
+
+  // Our own modified scroller
   function scrollComplete() {
     console.log("scrolling done");
-  }
 
-}
-fullPageScroll();
+    var sectionTops = [0, 1117, "2234"];
+    TweenLite.to(window, 0.5, { scrollTo: sectionTops[1], ease:Power2.easeOut });
+  }
+})();
