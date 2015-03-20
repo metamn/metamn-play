5dc3d4b3ff3dafe5845bb256918b686a6d6d79e6
cs <bartus.csongor@gmail.com>
2015-03-20 15:07:20 +0200

    Material design like shadow mixin added

--- site/components/framework/behavior/slider/slider.js
+++ site/components/framework/behavior/slider/slider.js
@@ -24,7 +24,7 @@ var slider = function(bulletsSelector, slidesSelector) {
 
   // Single navigation bullet
   function clickBulletSingle(bullet) {
-    // ??? 
+    // ???
   }
 
   // Multiple navigation bullets
