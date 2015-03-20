039bf30204592189697a7f555674e69ee902706c
cs <bartus.csongor@gmail.com>
2015-03-20 23:31:00 +0200

    Slider with switcher all  done

---
+++ site/components/framework/behavior/slider/--switcher/slider--switcher.js
@@ -0,0 +1,25 @@
+var sliderSwitcher = function(switcherClass, slidesClass) {
+  var bullet = document.querySelector(switcherClass);
+  var slides = document.querySelectorAll(slidesClass);
+
+  // Mark first slide active
+  slides[0].classList.add('active');
+
+  // Listen to the click event
+  bullet.addEventListener('click', clickBullet, false);
+
+  // Switch slides
+  function clickBullet() {
+    for (var i = 0; i < slides.length; i++ ) {
+      if (slides[i].classList.contains('active')) {
+        active = i;
+        break;
+      }
+    }
+    slides[active].classList.remove('active');
+
+    next = active + 1;
+    if (active == (slides.length - 1)) next = 0;
+    slides[next].classList.add('active');
+  }
+}
