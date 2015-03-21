f62f41d833e078af92c9ec0ee966a7ddf1ab66f4
cs <bartus.csongor@gmail.com>
2015-03-21 17:45:48 +0200

    Switcher positioning still not working

--- site/components/project/value/--interactions/__switcher/value--interactions__switcher.js
+++ site/components/project/value/--interactions/__switcher/value--interactions__switcher.js
@@ -3,7 +3,7 @@ sliderSwitcher('.value--interactions__switcher div', '.value--interactions__mock
 
 
 var positionSwitcher = function() {
-  var container = document.querySelector('.value--interactions__mockup .ios-device__screen');
+  var container = document.querySelector('.value--interactions__mockup');
   var x = container.clientWidth;
   var y = container.clientHeight;
 
@@ -14,16 +14,20 @@ var positionSwitcher = function() {
   var switcher_width = window.getComputedStyle(switcher, null).getPropertyValue("width");
   var switcher_height = window.getComputedStyle(switcher, null).getPropertyValue("height");
 
-  var x1 = x * 52.03/100 + parseFloat(padding.replace('px', '')) - parseFloat(switcher_width.replace('px', '')) / 2;
-  var y1 = y * 78.257/100 + parseFloat(switcher_height.replace('px', '')) / 2;
+  var x1 = 2 + x * 52.03125/100 + parseFloat(padding.replace('px', '')) - parseFloat(switcher_width.replace('px', '')) / 2;
+  var y1 = y * 67.6701/100 + parseFloat(switcher_height.replace('px', '')) / 2;
   y1 *= -1;
 
-  console.log('x1: ' + x1 + 'y1: ' + y1 + ' ');
+  console.log('x: ' + x + 'y: ' + y + ' ');
 
   switcher.style.top = y1 + 'px';
   switcher.style.left = x1 + 'px';
 }
 
 window.onload = function(e) {
+  positionSwitcher();
+}
+
+window.onchange = function(e) {
   positionSwitcher();
 }
